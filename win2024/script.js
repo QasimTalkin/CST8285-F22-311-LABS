04a110938b27f9f10d105fc47d20c1bcd28eba87
Qasim <qasim@Qasims-MacBook-Pro.local>
2024-03-12 19:21:59 -0400

    js part 1

---
+++ win2024/script.js
@@ -0,0 +1,40 @@
+
+let myArry = ["qas", "ere", "asdasd", 1]
+
+let daniel = {
+    age:17,
+    name:"DB",
+    canDrink:false
+}
+
+let michael = {
+    age:20,
+    name:"MD",
+    canDrink:true
+}
+
+
+let michael1 = {
+    age:20,
+    name:"MD",
+    canDrink:true
+}
+
+
+let michael2 = {
+    age:20,
+    name:"MD",
+    canDrink:true
+}
+
+
+let michael3 = {
+    age:20,
+    name:"MD",
+    canDrink:true
+}
+let students = [daniel, michael, michael1, michael2, michael3]
+
+for (var value of students) {
+console.log(value.age)
+}
